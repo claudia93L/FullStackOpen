c5ecd3540cc95d96e712ff520406ac7cdfaa6756
claudia93L <34545518+claudia93L@users.noreply.github.com>
2025-03-24 21:00:26 +0100

    1.3

---
+++ part1/courseinfo/CourseInfo/react-app-part1/src/App.jsx
@@ -0,0 +1,40 @@
+import Header from './components/Header';
+import Content from './components/Content';
+import Total from './components/Total';
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content
+        part1={part1.name}
+        part2={part2.name}
+        part3={part3.name}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+      ></Content>
+      <Total
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+      ></Total>
+    </div>
+  );
+};
+
+export default App;
