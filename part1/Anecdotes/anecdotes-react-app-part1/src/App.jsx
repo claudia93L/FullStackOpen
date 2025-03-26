f8fc420f1a944901510f4d5a12593e9374f52d55
claudia93L <34545518+claudia93L@users.noreply.github.com>
2025-03-26 14:02:25 +0100

    1.14 integrata selezione di aneddoti a parimerito di voti

--- part1/Anecdotes/anecdotes-react-app-part1/src/App.jsx
+++ part1/Anecdotes/anecdotes-react-app-part1/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from './components/Button';
+import Title from './components/Title';
 
 const App = () => {
   const initialAnecdotes = [
@@ -36,29 +37,63 @@ const App = () => {
   const [anecdotes, setAnecdotes] = useState(initialAnecdotes);
 
   const handleAnecdoteChange = () => {
+    // setto selected con un numero randoms
     setSelected(Math.floor(Math.random() * anecdotes.length));
   };
 
   const handleVotes = () => {
+    // copio l'oggetto
     const updatedAnecdotes = [...anecdotes];
+    // aumento il numero di voti per l'anecdote selezionato
     updatedAnecdotes[selected].votes += 1;
+    // aggiorno lo stato con i nuovi dati
     setAnecdotes(updatedAnecdotes);
   };
 
-  return (
-    <div>
-      <p>{anecdotes[selected].anecdote}</p>
-      {anecdotes[selected].votes === 0 ? (
-        <p>No votes yet.</p>
-      ) : (
-        <p>{anecdotes[selected].votes} votes received</p>
-      )}
+  // trovo l'aneddoto con il numero più grande di voti ciclando sull'oggetto
+  const maxVotes = Math.max(...anecdotes.map((anecdote) => anecdote.votes));
 
+  // valorizzo la variabile con gli aneddoti trovati in base all'uguaglianza rispetto al numero maggiore di voti
+  const mostVotedAnecdotes = anecdotes.filter(
+    (anecdote) => anecdote.votes === maxVotes
+  );
+
+  return (
+    <>
       <div>
-        <Button onClick={handleAnecdoteChange} btnName='Next anecdote'></Button>
-        <Button onClick={handleVotes} btnName='Place vote'></Button>
+        <Title title='Anecdote of the day'></Title>
+        <p>{anecdotes[selected].anecdote}</p>
+        {anecdotes[selected].votes === 0 ? (
+          <p>No votes yet.</p>
+        ) : (
+          <p>{anecdotes[selected].votes} votes received</p>
+        )}
+
+        <div>
+          <Button
+            onClick={handleAnecdoteChange}
+            btnName='Next anecdote'
+          ></Button>
+          <Button onClick={handleVotes} btnName='Place vote'></Button>
+        </div>
       </div>
-    </div>
+      <div>
+        <Title title='Anecdote with most votes'></Title>
+        /* se il voto massimo è maggiore di zero, allora ciclo sull'oggetto
+        degli aneddoti che vengono filtrati per numero massimo di voti */
+        {maxVotes > 0 ? (
+          mostVotedAnecdotes.map((el, index) => (
+            <div key={index}>
+              <p>{el.anecdote}</p>
+              <p>This anecdote has {el.votes} votes</p>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <p>No votes yet.</p>
+        )}
+      </div>
+    </>
   );
 };
 
