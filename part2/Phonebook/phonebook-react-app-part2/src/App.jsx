d8047befda8ea664f677162994b838a17281add0
claudia93L <34545518+claudia93L@users.noreply.github.com>
2025-03-27 13:01:02 +0100

    2.9

--- part2/Phonebook/phonebook-react-app-part2/src/App.jsx
+++ part2/Phonebook/phonebook-react-app-part2/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '32165466846' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchName, setSearchName] = useState('');
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
@@ -14,6 +18,17 @@ const App = () => {
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value);
   };
+
+  const handleSearchName = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  const personsToShow =
+    searchName.trim() === ''
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(searchName.toLowerCase())
+        );
 
   const checkEmptyInputs = () => {
     // controllo se i campi sono vuoti
@@ -53,6 +68,7 @@ const App = () => {
     const newPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
 
     setPersons(persons.concat(newPerson));
@@ -80,6 +96,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input type='text' value={searchName} onChange={handleSearchName} />
+      </div>
+      <br />
+      <h2>Add new person</h2>
       <form onSubmit={handleAddPerson}>
         <div>
           name: <input type='text' onChange={handleNewName} value={newName} />
@@ -98,9 +120,9 @@ const App = () => {
       <hr />
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => {
+        {personsToShow.map((person) => {
           return (
-            <li key={person.name}>
+            <li key={person.id}>
               {person.name}: {person.number}
             </li>
           );
