fe7b7877039e1edab3ed262c485525f54795ed87
claudia93L <34545518+claudia93L@users.noreply.github.com>
2025-03-27 11:25:18 +0100

    add name e inizio save person

--- part2/Phonebook/phonebook-react-app-part2/src/App.jsx
+++ part2/Phonebook/phonebook-react-app-part2/src/App.jsx
@@ -4,12 +4,21 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const addName = (event) => {
+    setNewName(event.target.value);
+    //console.log(newName);
+  };
+
+  const handleSavePerson = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSavePerson}>
         <div>
-          name: <input />
+          name: <input onChange={addName} />
         </div>
         <div>
           <button type='submit'>add</button>
